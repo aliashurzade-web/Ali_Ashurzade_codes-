3140fc8d135402788504f08dea49a317ab705cd8
aliashurzade-web <aliashurzade@gmail.com>
2026-06-15 18:02:29 +0400

    Sinif_isi

---
+++ D48/Sinif_isi/script.js
@@ -0,0 +1,23 @@
+console.log("Salam Qrup 11 !");
+
+let name = "Alma";
+
+let winner = "Heyva";
+
+let meyve = "Kelem";
+
+const tunar = 1293801;
+
+alert(" "+name+" "+winner+"");
+
+document.write(meyve)
+
+let ad = "Əli";
+
+var şəhər = "Bakı"
+
+var şəhər = "Barcelona"
+
+console.log(" "+şəhər+" ")
+
+alert("Salam, "+ad+"!")
