176a3e75680b466d1856d4c8d750d61adef0dcdd
aliashurzade-web <aliashurzade@gmail.com>
2026-06-18 09:50:53 +0400

    Ev_isi

---
+++ D48/Ev_isi/Ev_isi/app.js
@@ -0,0 +1,54 @@
+// 1. "Ən sevdiyim oyun" tapşırığı
+
+let favoriteGame = "GTA5";
+
+console.log("Mənim ən sevdiyim oyun " + favoriteGame + " oyunudur.");
+
+
+// 2. Dəyişməyən Gizli Kod
+
+const secretCode = 2938;
+
+// secretCode = 5555;
+
+// Xəta:
+// Error: sabit dəyişənə başqa təyinat verdikdə error verir
+
+
+// 3. Avtomobil Sərgisi
+
+var car = "Lamborghini";
+
+document.write("Sərgidə olan birinci maşın: " + car + "<br>");
+
+car = "BMW";
+
+document.write("Sərgidə olan ikinci maşın: " + car);
+
+
+// 4. Kosmonavtın Salamı
+
+let planet = "Mars";
+let spaceship = "Yıldırım";
+
+alert(spaceship + " gəmisi " + planet + " planetinə eniş etdi!");
+
+// 5 Dəyişənlərin Yarışı
+
+let point = 10;
+
+// let point = 20;  Xəta verəcək
+
+// Xəta:
+// Error: let də 2 dəyişən verəndə error verir 1 dənə olmalıdır
+
+
+var score = 10;
+var score = 20;
+
+// var ilə eyni adlı 2 dəyişən yaratmaq olar.
+
+console.log(score); // axırıncını götürəcək 20
+
+// var ilə eyni adlı 2 dəyişən yaratmaq olar.
+// let ilə eyni adlı 2 dəyişən yaratmaq olmaz
