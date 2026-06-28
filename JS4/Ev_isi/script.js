7eb2a16356a38fdd208b5c9b8a4f71ef7cf55b94
aliashurzade-web <aliashurzade@gmail.com>
2026-06-28 22:47:41 +0400

    Ev_isi

---
+++ JS4/Ev_isi/script.js
@@ -0,0 +1,33 @@
+
+let a = 12;
+let b = 9;
+
+if (b%3 == 0 && (2%3)>2 && (a+b)%4 == 1) {
+    console.log("Tunar doyulsun")
+}
+else{
+    console.log("Huseyn doyulsun");
+    
+}
+
+
+let c = 21;
+let d = 80;
+let e = 12;
+let f = 62;
+
+c/=6;
+++c;
+d/=2;
+d**=4;
+--d;
+e**=2;
+e/2;
+f/=2;
+
+if (c%4 > e && (f+d)%2 >1 || (c+e*f/d) > (a+b)/(a+b)) {
+    console.log("Ilkin doyulusun");
+}
+else{
+    console.log("Ziya çox pis doyulsun");
+}
