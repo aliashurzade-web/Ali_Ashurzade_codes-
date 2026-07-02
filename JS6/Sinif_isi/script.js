fbcf6d16f05c9eb8276a87c7fa76a3eab37ba94a
aliashurzade-web <aliashurzade@gmail.com>
2026-07-02 17:16:50 +0400

    Sinif_isi

---
+++ JS6/Sinif_isi/script.js
@@ -0,0 +1,117 @@
+let ay = +prompt("Hər hansısa ayın rəqəmini daxil edin")
+switch (ay) {
+    
+    case "1":
+    console.log("Yanvar");
+    break;
+
+    case "2":
+    console.log("Fevral");
+    break;
+
+    case "3":
+    console.log("Mart");
+    break;
+
+    case "4":
+    console.log("Aprel");
+    break;
+
+    case "5":
+    console.log("May");
+    break;
+
+    case "6":
+    console.log("Iyun");
+    break;
+
+    case "7":
+    console.log("Iyul");
+    break;
+
+    case "8":
+    console.log("Avqust");
+    break;
+
+    case "9":
+    console.log("Sentyabr");
+    break;
+
+    case "10":
+    console.log("Oktyabr");
+    break;
+
+    case "11":
+    console.log("Noyabr");
+    break;
+
+    case "12":
+    console.log("Dekabr");
+    break;
+
+    default:
+        console.log("Belə bir ay yoxdur")
+    break;
+}
+
+
+
+let işiqfor = +prompt("İşıqforun hər hansı 3 rəqəmdən birini daxil edin.")
+switch (işiqfor) {
+    
+    case "1":
+    console.log("Qırmızı");
+    break;
+
+    case "2":
+    console.log("Sarı");
+    break;
+
+    case "3":
+    console.log("Yaşıl");
+    break;
+
+    default:
+        console.log("3 dən çox rəng yoxdur")
+    break;
+}
+
+
+let planet = +prompt("Planetin sıra nömrəsini daxil et:");
+switch (planet) {
+    case 1:
+    console.log("Neptun");
+    break;
+
+    case 2:
+    console.log("Mars");
+    break;
+
+    case 3:
+    console.log("Yer planeti");
+    break;
+
+    case 4:
+    console.log("Merkur");
+    break;
+
+    case 5:
+    console.log("Venera");
+    break;
+
+    case 6:
+    console.log("Saturn");
+    break;
+
+    case 7:
+    console.log("Uran");
+    break;
+
+    case 8:
+    console.log("Yupiter");
+    break;
+
+    default:
+        console.log("8 dən çox planet yoxdur");
+    break;
+}
