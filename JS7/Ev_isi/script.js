735b79ba462c7f0ae0a6f88ce56668cb3e4d396b
aliashurzade-web <aliashurzade@gmail.com>
2026-07-05 22:58:11 +0400

    Ev_isi

---
+++ JS7/Ev_isi/script.js
@@ -0,0 +1,175 @@
+//1
+
+let qəhreman = prompt("Ən sevdiyiniz superqəhrəmanın adını yazın:");
+
+switch (qəhreman) {
+    case "Spiderman":
+    console.log("Top möhkəm atanda zaboru dırmaşıb topu götürə bilir.");
+    break;
+
+    case "Batman":
+    console.log("Batmobil-də 2 təkər qaldırır");
+    break;
+
+    case "Supermen":
+    console.log("Uçur");
+    break;
+
+    default:
+    console.log("Dediyiniz qəhrəma-ı mən tanımıram");
+}
+
+
+let hava = prompt("Hava necədir? (mülayım, yağıntılı, boran)");
+
+switch (hava){
+    case "mülayim":
+    console.log("Qoluaçıq geyinə bilərsiz");
+    break;
+
+    case "yağıntılı":
+    console.log("Zontik götür.");
+    break;
+
+    case "boran":
+    console.log("Şərf bağlayın, qalın kofta,şalvar, və mayka geyin");
+    break;
+
+    default:
+    console.log("Belə hava vəziyyəti tanınmadı.");
+}
+
+
+
+let planet = prompt("Hansı planetə uçmaq istəyirsiniz? (neptun, yupiter, uran)");
+
+switch (planet) {
+    case "Neptun":
+    console.log("Neptun-a gedə bilsən elon mask sənə yalvaracaq");
+    break;
+
+    case "yupiter":
+    console.log("Yupiterdə özünü kök hiss eliyəcəksən");
+    break;
+
+    case "uran":
+    console.log("Uran heç bilmirəm uran hansıdı elə belə yazdım");
+    break;
+
+    default:
+    console.log("Planet siyahıda tapılmadı");
+}
+
+
+
+let peşə = prompt("Böyüyəndə nə olmaq istəyirsən (proqramist, həkim, vəkil, F1 SÜRÜCÜ)");
+
+switch (peşə){
+    case "həkim":
+    console.log("Xəsdə insanlara kömək edəcəksən.");
+    break;
+
+    case "proqramist":
+    console.log("Gözünüz kor olacaq");
+    break;
+
+    case "vəkil":
+    console.log("Başqalarının davasında kiməsə haqq herəcəksən");
+    break;
+
+    case "F1 SÜRÜCÜ":
+    console.log("Max Verstappen");
+    break;
+
+    default:
+    console.log("Ümid edirəm arzunuza çatacaqsınız.");
+}
+
+
+let meyvə = prompt("Sevdiyiniz meyvəni yazın (alma, banan, çiyələk, limon)");
+
+switch (meyvə){
+    case "alma":
+    console.log("Şirin alma Turş alma və Qızıl Əhmədi Alma");
+    break;
+
+    case "banan":
+    console.log("Şirin və dadlıdı");
+    break;
+
+    case "çiyələk":
+    console.log("Qırmızı və şirin dadı var");
+    break;
+
+    case "limon":
+    console.log("No reaction challange");
+    break;
+
+    default:
+    console.log("Bu meyvə səbətdə yoxdur.");
+}
+
+
+
+let heyvan = prompt("Hansı heyvanı yedizdirmək istəyirsiniz? (meymun, pişik, it)");
+
+switch (heyvan){
+    case "pişik":
+    console.log("Pişik ət balıq hər şey yeyir");
+    break;
+
+    case "it":
+    console.log("İt mal əti yeyir");
+    break;
+
+    case "meymun":
+    console.log("Banan yeyur");
+    break;
+
+    default:
+    console.log("Bu heyvan haqqında məlumat yoxdur.");
+}
+
+
+
+let qəhrəman = prompt("Hansı cizgi filmi qəhrəmanı ilə görüşmək istəyirsiniz? (şrek, Qardan adam, sualtı qədeşlər)");
+
+switch (qəhrəman){
+    case "şrek":
+    console.log("Şrek ilə görüşəndə onu öz bataqlığına apar");
+    break;
+
+    case "Qardan adam":
+    console.log("Günün altında yanında qardan adama baxanda birdə görəcəksən sudu");
+    break;
+
+    case "sualtı qədeşlər":
+    console.log("Çox gülməlidirlər və səni yeyəcəklər");
+    break;
+
+    default:
+    console.log("Bu qəhrəman hazırda tətildədir.");
+}
+
+let neqliyyat = prompt("Nə ilə səyahət etmək istəyirsiniz? (gəmi, təyyarə, qatar, maşın)");
+
+switch (neqliyyat){
+    case "gəmi":
+    console.log("Bu nəqliyyat dənizdə hərəkət edir.");
+    break;
+
+    case "təyyarə":
+    console.log("Təyyarə çox tez hərəkət edir");
+    break;
+
+    case "qatar":
+    console.log("Qatar çox səs küy olur");
+    break;
+
+    case "maşın":
+    console.log("Maşınla gedəndə çox uzun çəkir");
+    break;
+
+    default:
+    console.log("Bu nəqliyyat yoxdur. Piyada gəzmək olar.");
+}
