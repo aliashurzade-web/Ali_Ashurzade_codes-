89b7b544be450f4e959a26c7f062a36950fd61d4
aliashurzade-web <aliashurzade@gmail.com>
2026-07-06 14:01:49 +0400

    Sinif_isi

---
+++ JS7/Sinif_isi/script.js
@@ -0,0 +1,12 @@
+function sayHi(params) {
+    alert("Salam, Dünya!");
+}
+
+let ad = prompt("Sizin Adınız nədir?");
+
+function salamlamaq(ad){
+    console.log("Salam "+ad+ "!");
+}
+
+salamlamaq(ad);
+
