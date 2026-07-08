f700cb868ce75dea6acd45838677068420720027
aliashurzade-web <aliashurzade@gmail.com>
2026-07-08 14:05:46 +0400

    Sinif_isi

---
+++ JS8/Sinif_isi/script.js
@@ -0,0 +1,8 @@
+let metn = "   salam, proqramçı dostlar!   ";
+let metn1 = "nicat";
+
+metn = metn.trim().toUpperCase();
+metn1 = metn1.charAt(0).toUpperCase() + metn1.slice(1);
+
+console.log(metn);
+console.log(metn1);
