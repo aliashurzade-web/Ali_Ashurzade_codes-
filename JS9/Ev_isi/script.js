f6007c92c3ad9d19e04c5e0fd02136d2efbb69a2
aliashurzade-web <aliashurzade@gmail.com>
2026-07-09 09:43:07 +0400

    Ev_isi

---
+++ JS9/Ev_isi/script.js
@@ -0,0 +1,51 @@
+
+let proqramist = "proqramist";
+proqramist = proqramist.toUpperCase();
+console.log(proqramist);
+
+
+let CODING = "CODING";
+CODING = CODING.toLowerCase();
+console.log(CODING);
+
+
+let proqramlaşdırma = "Mən proqramlaşdırmanı sevirəm";
+proqramlaşdırma = proqramlaşdırma.indexOf("sev");
+console.log(proqramlaşdırma);
+
+
+let js= "JAVASCRİPT çox maraqlıdır";
+js = js.includes("maraqlıdır");
+console.log(js);
+
+
+let salam = "Salam, Dosdum!";
+salam = salam.replace ("Dosdum", "Dünya");
+console.log(salam);
+
+
+let kodlaşdırmaq = "   Kod Yazmaq Əyləncəlidir.  ";
+kodlaşdırmaq = kodlaşdırmaq.trim(" ");
+console.log(kodlaşdırmaq);
+
+
+let frontend = "frontend";
+frontend = frontend.slice(0,6);
+console.log(frontend);
+
+
+let html_css_js = "HTML, CSS, JAVASCRIPT";
+html_css_js = html_css_js.split(", ");
+console.log(html_css_js);
+
+
+let Kitabxana ="Kitabxana";
+Kitabxana = Kitabxana.slice([-3]);
+console.log(Kitabxana);
+
+
+let Bakı = "   Bakı gözəl şəhərdir.  ";
+Bakı = Bakı.trim(" ");
+Bakı = Bakı.toUpperCase();
+Bakı = Bakı.split(" ");
+console.log(Bakı);
